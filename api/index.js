531705d143be1bcc5fce61b8785c09d1794ac73f
th3ghote-blip <th3ghote@gmail.com>
2025-09-02 03:42:47 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,10 @@
+// api/index.js
+module.exports = (req, res) => {
+  res.status(200).json({
+    ok: true,
+    path: req.url,
+    method: req.method,
+    time: new Date().toISOString(),
+  });
+};
 [object Object]
